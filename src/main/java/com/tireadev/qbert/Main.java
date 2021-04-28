712fb63a901c91fc3872a58c393f8d64ec808512
jinfra <72431407+fairaislj@users.noreply.github.com>
2021-04-28 14:39:24 +0200

    Main menu sprites done

--- src/main/java/com/tireadev/qbert/Main.java
+++ src/main/java/com/tireadev/qbert/Main.java
@@ -5,8 +5,7 @@ import com.tireadev.shadowengine.ShadowEngine;
 
 import java.util.Locale;
 
-import static com.tireadev.qbert.Main.path_atlas;
-import static com.tireadev.qbert.Main.path_prefix;
+import static com.tireadev.qbert.Main.*;
 
 public class Main extends ShadowEngine {
 
@@ -115,20 +114,56 @@ class MapScene extends Scene {
 class MainMenuScene extends Scene{
 
     ShadowEngine se;
-    byte[][] chars = new byte[26][];
+
 
     public MainMenuScene(ShadowEngine instance) {
         super(instance);
     }
 
-     public void onAwake(){
-        se = instance;
-        for (int i = 0; i < 26; i++) chars[i] = se.loadImage(path_atlas,128+8*i,8,8,8);
+    byte[] ultraGames;
+    byte[] qbertTitle;
+    byte[] bertHimself;
+    byte[][] chars = new byte['Z'-44+1][];
+    //byte[][] nums = new byte[10][];
 
-     }
+
+    public void onAwake() {
+        se = instance;
+        ultraGames = se.loadImage(path_atlas,344,0,8*20,8*4);
+        qbertTitle = se.loadImage(path_atlas,344-8,32,16*10,16*3);
+        for (int i = '0'-44; i <= '9'-44; i++) chars[i] = se.loadImage(path_atlas,128+8*(i-'0'+44),64,8,8); // řádek 1
+        for (int i = 'A'-44; i <= 'Z'-44; i++) chars[i] = se.loadImage(path_atlas,128+8*(i-'A'+44),64+8,8,8); // řádek 2
+        chars[0] = se.loadImage(path_atlas, 128+160, 64, 8, 8); // ,
+        chars[2] = se.loadImage(path_atlas, 128+168, 64, 8, 8); // .
+        bertHimself = se.loadImage(path_atlas,0,0,16,16);
+    }
+
 
     @Override
-    public void onUpdate(float deltaTime) {
-        se.drawText("Pavel", 32, 32, chars, 65, 4, true);
+    public void onUpdate(float deltaTime){
+        se.drawImage((5*16+8)*scale,(32-8)*scale,ultraGames,scale);
+        se.drawImage((3*16)*scale,(16*3)*scale,qbertTitle,scale);
+        se.drawText("Play Select", (16*6-8)*scale, (16*6)*scale, chars, 44, scale, true);
+        se.drawText("Play", (16*7)*scale, (16*7)*scale, chars, 44, scale, true);
+        se.drawText("Quit", (16*7)*scale, (16*8)*scale, chars, 44, scale, true);
+        se.drawImage((5*16)*scale,(16*7)*scale,bertHimself,scale);
+        se.drawText("TM AND  0  1989", 5*16*scale, 10*16*scale, chars, 44, scale, true);
+        se.drawText("KONAMI INDUSTRY CO.,LTD.", 2*16*scale, (10*16*scale)+16, chars, 44, scale, true);
+        se.drawText("LICENSED BY", 6*16*scale, 11*16*scale, chars, 44, scale, true);
+        se.drawText("NINTENDO OF AMERICA INC.", 2*16*scale, (11*16*scale)+16, chars, 44, scale, true);
+        se.drawText("ULTRA GAMES IS A REGISTERED ", 16*scale, (12*16*scale)+16, chars, 44, scale, true);
+        se.drawText("TRADEMARK OF ULTRA SOFTWARE", 16*scale, 13*16*scale, chars, 44, scale, true);
+        se.drawText("CORPORATION.", 16*scale, (13*16*scale)+16, chars, 44, scale, true);
+
+
+
+
+        //byte[][] chars = new byte['Z'-44+1][];
+
+        //for (int i = 'A'-44; i <= 'Z'-44; i++) chars[i] = se.loadImage(path_atlas,128+8*(i-'A'+44),64+8,8,8);
+
+        //se.drawText("GAME OVER", (16*6-8)*scale, (16*8)*scale, chars, 44, scale, true);
+        //se.drawText("Continue A", (16*7)*scale, (16*9)*scale, chars, 44, scale, true);
+        //se.drawText("Game end B", (16*7)*scale, (16*10)*scale, chars, 44, scale, true);
     }
 }
