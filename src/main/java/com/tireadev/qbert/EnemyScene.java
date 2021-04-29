ffe0cce90bea4fa4114689c59606d2bebfaae8de
Jonáš Procházka <prochazkaj@spseplzen.cz>
2021-04-29 20:16:26 +0200

    Basic enemy tilemap movement

--- src/main/java/com/tireadev/qbert/EnemyScene.java
+++ src/main/java/com/tireadev/qbert/EnemyScene.java
@@ -3,24 +3,35 @@ package com.tireadev.qbert;
 import com.tireadev.shadowengine.Scene;
 import com.tireadev.shadowengine.ShadowEngine;
 
-import static com.tireadev.qbert.Main.atlas;
+import static com.tireadev.qbert.Main.*;
+import static com.tireadev.qbert.Main.scale;
 
 public class EnemyScene extends Scene {
 
     ShadowEngine se;
 
-    final int enemyStartX = 7 * 32 + 16;
-    final int enemyStartY = 3 * 32 - 24;
+    final int enemyStartX = 3;
+    final int enemyStartY = 0;
     int enemyX = enemyStartX;
     int enemyY = enemyStartY;
-    int xDirection;
-    byte[][] enemies = new byte[4][];
-    final int speed = 60;
-    int timer = 0;
-    final int rows = 7;
-    int verticalPosition = 1;
-    int waitTime = (int)(Math.random() * 4) + 2;
-    int wait = 0;
+    final int startEo = 0; // 0 = even, 1 = odd
+    int eo = startEo;
+    int enemyDirection; // 0 = left, 1 = right
+    int delay = 0;
+
+    byte[][] enemy = new byte[4][];
+
+    static final byte mapWidth = 7;
+
+    static final byte[] enemies = new byte[] {
+            0,0,0,1,0,0,0,
+             0,0,0,0,0,0,0,
+            0,0,0,0,0,0,0,
+             0,0,0,0,0,0,0,
+            0,0,0,0,0,0,0,
+             0,0,0,0,0,0,0,
+            0,0,0,0,0,0,0
+    };
 
     public EnemyScene(ShadowEngine instance) {
         super(instance);
@@ -38,42 +49,63 @@ public class EnemyScene extends Scene {
 
     @Override
     public void onAwake() {
-        enemies[0] = se.getSubImage(atlas, 5*16, 16, 16, 16);
+        enemy[1] = se.getSubImage(atlas, 5*16, 1*16, 16, 16);
+        enemy[0] = se.getSubImage(atlas, 6*16, 1*16, 16, 16);
     }
 
     @Override
     public void onUpdate(float v) {
-        if(timer == speed){
-            if(wait == waitTime){
-                if(verticalPosition != rows){
-                    this.xDirection = (int)(Math.random()*2);
-                    switch(xDirection){
-                        case 0: this.enemyX = this.enemyX + Main.tile;
-                            break;
-                        case 1: this.enemyX = this.enemyX - Main.tile;
-                    }
 
-                    this.enemyY = this.enemyY + Main.tile + (Main.tile / Main.scale);
-                    verticalPosition++;
-
-                }else{
-                    resetEnemyXY();
-                    verticalPosition = 1;
-                    wait = 0;
-                    waitTime = (int)(Math.random() * 2) + 2;
-
-                }
+        if(delay == 60){
+            if(enemyY == 6){
+                this.enemies[enemyY * 7 + enemyX] = 0;
+                resetEnemyXY();
+                this.enemies[enemyY * 7 + enemyX] = 1;
             }else{
-                wait++;
-            }
-            timer = 0;
+                this.enemyDirection = (int)(Math.random()*2);
+                this.enemies[enemyY * 7 + enemyX] = 0;
+                if(enemyDirection == 1){ // 1 = right, 0 = left
+                    enemyY++;
+                    if(eo == 1){ // 0 = even, 1 = odd
+                        enemyX++;
+                        eo = 0;
+                    }else{
+                        eo = 1;
+                    }
+                }else{
+                    enemyY++;
+                    if(eo == 0){
+                        enemyX--;
+                        eo = 1;
+                    }else{
+                        eo = 0;
+                    }
+                }
 
-        }else{
-            timer++;
+                this.enemies[enemyY * 7 + enemyX] = 1;
+            }
+            delay = 0;
+        }else {
+            delay++;
         }
 
-        if(wait == waitTime){
-            se.drawImage(this.enemyX, this.enemyY, enemies[0], Main.scale);
+        for (int y = 0; y < mapWidth; y++) {
+            for (int x = 0; x < mapWidth; x++) {
+                int val = enemies[y * mapWidth + x];
+                if (val > 0) {
+
+                    int ox = 0, oy = tile*3/4 * scale;
+                    if (y % 2 == 1) ox = tile/2 * scale;
+
+                    ox += tile/2 * scale;
+
+                    int tx = x * tile * scale + ox;
+                    int ty = y * oy + (tile * scale * 5/4);
+
+                    if (val >= enemy.length) val = 0;
+                    se.drawImage(tx+16, ty-8, enemy[val], scale);
+                }
+            }
         }
     }
 }
