1c529ed981c7552db5554594c6b5911aa171a100
Leo Fryč <frycl@spseplzen.cz>
2021-04-29 18:54:43 +0200

    Movement Planning

--- src/main/java/com/tireadev/qbert/QbertScene.java
+++ src/main/java/com/tireadev/qbert/QbertScene.java
@@ -9,14 +9,23 @@ public class QbertScene extends Scene {
 
     ShadowEngine se;
 
-    final int qbtstartX = 7 * 32 + 16;
-    final int qbtstartY = 3 * 32 - 24;
+    final int qbtstartX = 7 * 32 + 16; // 3
+    final int qbtstartY = 3 * 32 - 24; // 0
 
     int qbtX = qbtstartX;
     int qbtY = qbtstartY;
     int xDirection;
     byte[][] qbrt = new byte[4][];
 
+    static final byte[] tilemap = new byte[] {
+            0,0,0,0,0,0,0,
+             0,0,0,0,0,0,0,
+            0,0,0,1,0,0,0,
+             0,0,0,0,0,0,0,
+            0,0,0,0,0,0,0,
+             0,0,0,0,0,0,0,
+            0,0,0,0,0,0,0
+    };
 
     public QbertScene(ShadowEngine instance) {
         super(instance);
@@ -35,17 +44,38 @@ public class QbertScene extends Scene {
 
     @Override
     public void onUpdate(float v) {
+
+        // změnit číslo na pozici XY na 0
+
+        // x: 0 → w
+        // y: 0 ↓ h
+
         if (instance.keyPressed('W')){
-            System.out.println("W was pressed");
+//            System.out.println("W was pressed");
+
+            // pokud lichý řádek, posuň se nahoru a doleva
+            // pokud sudý řádek, pokuň se nahoru
         }
         else if (instance.keyPressed('S')){
-            System.out.println("S was pressed");
+//            System.out.println("S was pressed");
+
+            // pokud lichý řádek, posuň se nahoru
+            // pokud sudý řádek, posuň se nahoru a doprava
         }
         else if (instance.keyPressed('A')){
-            System.out.println("A was pressed");
+//            System.out.println("A was pressed");
+
+            // pokud lichý řádek, posuň se dolů a doleva
+            // pokud sudý řádek, posuň se dolů
         }
         else if (instance.keyPressed('D')){
-            System.out.println("D was pressed");
+//            System.out.println("D was pressed");
+
+            // pokud lichý řádek, posuň se dolů
+            // pokud sudý řádek, posuň se dolů a doprava
         }
+
+        // tilemap[y * 7 + x];
+        // změnit číslo na pozici XY na 1
     }
 }
