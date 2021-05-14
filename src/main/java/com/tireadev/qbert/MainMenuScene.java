e9d51ac70cc17228b8a1872b7daec9c0c17e16d2
TheTirea <30987354+TheTirea@users.noreply.github.com>
2021-05-14 11:50:45 +0200

    added keys WS to menu controls

--- src/main/java/com/tireadev/qbert/MainMenuScene.java
+++ src/main/java/com/tireadev/qbert/MainMenuScene.java
@@ -9,20 +9,20 @@ public class MainMenuScene extends Scene {
     byte[] ultraGames;
     byte[] qbertTitle;
     byte[] bertHimself;
-    byte[][] chars = new byte['Z'-44+1][];
+    byte[][] chars = new byte['Z' - 44 + 1][];
     public byte cursorPosition = 0;
 
-
     public void onAwake() {
-        ultraGames = getSubImage(atlas,344,0,8*20,8*4);
-        qbertTitle = getSubImage(atlas,344-8,32,16*10,16*3);
-        for (int i = '0'-44; i <= '9'-44; i++) chars[i] = getSubImage(atlas,128+8*(i-'0'+44),64,8,8); // řádek 1
-        for (int i = 'A'-44; i <= 'Z'-44; i++) chars[i] = getSubImage(atlas,128+8*(i-'A'+44),64+8,8,8); // řádek 2
-        chars[0] = getSubImage(atlas, 128+160, 64, 8, 8); // ,
-        chars[2] = getSubImage(atlas, 128+168, 64, 8, 8); // .
-        bertHimself = getSubImage(atlas,0,0,16,16);
+        ultraGames = getSubImage(atlas, 344, 0, 8 * 20, 8 * 4);
+        qbertTitle = getSubImage(atlas, 344 - 8, 32, 16 * 10, 16 * 3);
+        for (int i = '0' - 44; i <= '9' - 44; i++)
+            chars[i] = getSubImage(atlas, 128 + 8 * (i - '0' + 44), 64, 8, 8); // řádek 1
+        for (int i = 'A' - 44; i <= 'Z' - 44; i++)
+            chars[i] = getSubImage(atlas, 128 + 8 * (i - 'A' + 44), 64 + 8, 8, 8); // řádek 2
+        chars[0] = getSubImage(atlas, 128 + 160, 64, 8, 8); // ,
+        chars[2] = getSubImage(atlas, 128 + 168, 64, 8, 8); // .
+        bertHimself = getSubImage(atlas, 0, 0, 16, 16);
     }
-
 
     @Override
     public void onUpdate(float deltaTime) {
@@ -39,22 +39,17 @@ public class MainMenuScene extends Scene {
         drawText("TRADEMARK OF ULTRA SOFTWARE", 16 * scale, 13 * 16 * scale, chars, 44, scale, true);
         drawText("CORPORATION.", 16 * scale, (13 * 16 * scale) + 16, chars, 44, scale, true);
 
-
         // right 262
         // left 263
         // down 264
         // up 265
 
-        if(keyPressed(264) && (cursorPosition == 0)) cursorPosition++;
-        if(keyPressed(265) && (cursorPosition == 1)) cursorPosition--;
+        if ((keyPressed(264) || keyPressed('S')) && (cursorPosition == 0))
+            cursorPosition++;
+        if ((keyPressed(265) || keyPressed('W')) && (cursorPosition == 1))
+            cursorPosition--;
 
-
-        drawImage((5* 16+8) * scale, (16 * (7+cursorPosition )) * scale, bertHimself, scale);
-
-
-
-
-
+        drawImage((5 * 16 + 8) * scale, (16 * (7 + cursorPosition)) * scale, bertHimself, scale);
 
     }
 }
