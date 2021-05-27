fec7a33b0b654cf4613c793af5006a60e7dea16a
Jonáš Procházka <jonasprochaz@gmail.com>
2021-05-27 20:38:37 +0200

    enemy timer

--- src/main/java/com/tireadev/qbert/EntityScene.java
+++ src/main/java/com/tireadev/qbert/EntityScene.java
@@ -15,15 +15,19 @@ public class EntityScene extends Scene{
     byte[][] qbertSprites      = new byte[8][];
     byte[][] redBallSprites    = new byte[8][];
     byte[][] purpleBallSprites = new byte[8][];
-    
+
     ArrayList<Entity> entities = new ArrayList<Entity>();
-    
+
     public static byte[] tilemap = new byte[7*7];
-    
+
     static boolean qbertJumped = false;
-    
+    boolean enemySpawned = false;
+    int random = (int)(Math.random() * 2) + 60;
+
+    int checker = 0;
+
     Entity qbert;
-    
+
     @Override
     public void onAwake() {
         for (int ii = 0; ii < 8; ii++) {
@@ -31,36 +35,48 @@ public class EntityScene extends Scene{
             redBallSprites   [ii] = getSubImage(atlas, 16*(ii%2)   , 16, 16, 16);
             purpleBallSprites[ii] = getSubImage(atlas, 16*(ii%2)+64, 16, 16, 16);
         }
-        
+
         qbert = new Qbert(new Vec2i(2, 3), qbertSprites);
-        
+
         entities.add(qbert);
     }
-    
+
     public void clearEnemies() {
         entities.clear();
+        enemySpawned = false;
         entities.add(qbert);
     }
 
     @Override
     public void onUpdate(float deltaTime) {
         tilemap = new byte[7*7];
+
+        //if (keyPressed('P')) entities.add(new Ball(new Vec2i(3, 0), redBallSprites));
         
-        if (keyPressed('P')) entities.add(new Ball(new Vec2i(3, 0), redBallSprites));
-        
+        if(checker == random){
+            entities.add(new Ball(new Vec2i(3, 0), redBallSprites));
+            random = (int)(Math.random() * 60 * 10);
+            enemySpawned = true;
+            checker = 0;
+        }else{
+            checker++;
+        }
+
+
         for (int ii = entities.size()-1; ii >= 0; ii--) {
             Entity temp = entities.get(ii);
-        
+
             temp.update(deltaTime);
-            
+
             if (temp.type == EntityType.ENEMY && temp.pos.y > 6) {
                 entities.remove(ii);
+                enemySpawned = false;
                 continue;
             }
-            
+
             tilemap[temp.pos.y * mapWidth + temp.pos.x] = (byte)(ii + 1);
         }
-    
+
         for (int y = 0; y < mapWidth; y++) {
             for (int x = 0; x < mapWidth; x++) {
                 int val = tilemap[y * mapWidth + x];
@@ -95,7 +111,7 @@ class Qbert extends Entity {
     public void update(float deltaTime) {
         dir.x = 0;
         dir.y = 0;
-    
+
         if (       (keyPressed('W') || keyPressed(KEY_UP))    && pos.y > 0 && pos.x >= 0) {
             dir.x = -1;
             dir.y = -1;
@@ -109,19 +125,19 @@ class Qbert extends Entity {
             dir.x = 1;
             dir.y = -1;
         }
-        
+
         Vec2i prevPos = new Vec2i(pos);
-        
+
         move();
-        
+
         qbertJumped = (pos.x != prevPos.x || pos.y != prevPos.y);
     }
-    
+
     @Override
     public void spawn(){
         pos.x = 3;
-        pos.y = 0;
-        
+        pos.y = 1;
+
         dir.x = 0;
         dir.y = 0;
     }
@@ -140,28 +156,28 @@ class Ball extends Entity {
     public void update(float deltaTime) {
         dir.x = 0;
         dir.y = 0;
-    
+
         if (delay < 1) {
             delay += deltaTime;
             return;
         }
-        
+
         dir.x = (int)(Math.random()*2)-1;
         dir.y = 1;
-    
+
         delay = 0;
-        
+
         move();
     }
-    
+
     @Override
     public void spawn(){
         pos.x = 3;
         pos.y = 0;
-        
+
         dir.x = 0;
         dir.y = 0;
-        
+
         delay = 0;
     }
 }
